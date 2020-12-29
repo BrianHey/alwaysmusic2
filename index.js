d6c47247fdc4953a3a27c14e567370fec02326b2
Brian Habib <brianhft20@gmail.com>
2020-12-29 14:26:57 -0300

    agregada la configuracion

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const ingresarEstudiante = require("./nuevoUsuario");
 const { consultar, consultarRut } = require("./consulta");
 const actualizar = require("./actualizarRegistro");
 const { eliminarEstudiante } = require("./delete");
-const config = {};
 
+const config = {
+  user: "postgres",
+  host: "localhost",
+  password: "postgres",
+  database: "alwaysmusic",
+  port: 5432,
+  max: 20,
+  idleTimeoutMillis: 5000,
+  connectionTimeoutMillis: 2000,
+};
 const pool = new Pool(config);
+
+pool.connect((err, client, release) => {
+  try {
+
+    
+
+  } catch (e) {
+  } finally {
+    release();
+    pool.end();
+  }
+});
