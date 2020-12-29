2a056d79b258c1e28efd80d4cfcded0b274c3295
Cristian <cristian.morenos@outlook.com>
2020-12-29 11:53:03 -0300

    funcion eliminar estudiante

---
+++ delete.js
@@ -0,0 +1,18 @@
+const eliminarEstudiante = async (rut) => {
+    const consulta = {
+        text: "DELETE FROM estudiantes where rut = '$1'",
+        values: rut,
+        rowMode: "array",
+        name: "eliminar-estudiante"
+    }
+    try {
+        const res = await pool.query(consulta)
+        res.rowCount > 1 ? console.log("Estudiante eliminado") : console.log("El rut ingresado no existe, vuelve a intentar")
+    } catch (error) {
+        console.log(error.code)
+    }
+}
+
+module.exports = {
+    eliminarEstudiante
+}
