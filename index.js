583f86fbd65aff9ab36f5806d1c7da880205bc9f
Brian Habib <brianhft20@gmail.com>
2020-12-29 12:50:13 -0300

    importado el actualizar

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 const ingresarEstudiante = require("./nuevoUsuario");
 const { consultar, consultarRut } = require("./consulta");
-
+const actualizar = require("./actualizarRegistro")
 const config = {};
 
 const pool = new Pool(config);
